e2d80e546c15ec2834421712d3b5aed6b51da4d8
albus357 <876072684@qq.com>
2024-08-02 13:47:59 +0800

    chore: version update

--- packages/core/components.ts
+++ packages/core/components.ts
@@ -1,6 +1,6 @@
-import { ErButton } from "@toy-element/components"
+import { ErButton, ErButtonGroup, ErIcon } from "@toy-element/components"
 import type { Plugin } from "vue"
 
 export default [
-    ErButton
+    ErButton, ErIcon, ErButtonGroup
 ] as Plugin[];
